aeca2a198334f68a232b0670694c17e3171ced6e
Elisa <elisacanyelles@gmail.com>
2021-10-27 16:45:13 +0200

    added dropdown box
    dropdown box added + align with title.
    We still need to add the flags using the country flags icons

--- src/components/WhereToWatchCountry.js
+++ src/components/WhereToWatchCountry.js
@@ -4,22 +4,48 @@
  * 
  */
 
-import styled from 'styled-components' 
+import styled from 'styled-components'
+
+
 
 // Styles
+const Container =styled.div`
+display: flex;
+align-items: center;
+justify-content: space-around;
 
-const Title = styled.h4`
+` 
+const Title = styled.h3`
 
+` 
+const DisplayOptions = styled.select`
+padding:5px 90px 5px 0px;
+font-size: 15px;
+text-align: left;
 ` 
 
 // Component
 
 const WhereToWatchCountry = () => {
     return (
-        <div>
+        <Container>
             <Title>Where to watch</Title>
-        </div>
+        <DisplayOptions> 
+          <option>Spain</option>
+          <option>France</option>
+           <option>Germany</option>
+         </DisplayOptions>
+        </Container>
     )
+
 }
 
+
+
 export default WhereToWatchCountry
+{/* <DisplayOptions >
+                <option >Spain</option>
+                <option >Spain</option>
+                <option>Spain</option>
+                <option>Spain</option>
+            </DisplayOptions> */}
