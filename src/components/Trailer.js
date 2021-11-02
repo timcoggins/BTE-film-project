6adc70262e77651151e2a00fecfd615a752ac769
Eleonora <eleonora1weber@gmail.com>
2021-11-02 21:45:40 +0100

    added some logic to trailer component

--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -3,23 +3,42 @@
  * Shows the trailer for a movie
  * 
  */
-
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react"
+import axios from "axios";
 import styled from 'styled-components' 
 
 // Style
 
 const VideoLink = styled.img`
 
-` 
+`
+
 // Component
 
 const Trailer = () => {
+
+    const { id } = useParams();
+    const [trailerData, setTrailerData] = useState()
+    const url =
+      "https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=<<api_key>>&language=en-US"
+
+    useEffect(() => {
+        axios
+            .get(url)
+            .then((response) => setTrailerData(response.data))
+            .catch((error) => console.log(error));
+    }, [id])
+
+
+    useEffect(() => console.log(trailerData), [trailerData]);
+
     return (
-        <div>
+        <>
             Trailer
              <VideoLink src=""/>
-        </div>
+        </>
     )
 }
 
-export default Trailer
+export default Trailer;
