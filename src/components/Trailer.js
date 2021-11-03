0ff7e5a53287d5745640d569c425bdce8476f6e6
Eleonora <eleonora1weber@gmail.com>
2021-11-03 15:35:10 +0100

    BTE 40 and 41
    added some logic to trailer component

--- src/components/Trailer.js
+++ src/components/Trailer.js
@@ -11,7 +11,7 @@ import styled from 'styled-components'
 // Style
 
 const VideoLink = styled.img`
-
+width: 360px;
 `
 
 // Component
@@ -21,24 +21,41 @@ const Trailer = () => {
     const { id } = useParams();
     const [trailerData, setTrailerData] = useState()
     const url =
-      "https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=<<api_key>>&language=en-US"
+      `https://api.themoviedb.org/3/movie/${id}/videos?api_key=f60b20c74e47d524d562b3d0b29f6aeb`;
+
+    const checkTrailer = (data) => {
+        if (data.results[0].site === "YouTube") {
+            console.log("video is on youtube")
+             setTrailerData(data);
+        } 
+    }
+    
 
     useEffect(() => {
         axios
             .get(url)
-            .then((response) => setTrailerData(response.data))
+            .then((response) => checkTrailer(response.data)) 
             .catch((error) => console.log(error));
-    }, [id])
+    }, [])
 
 
     useEffect(() => console.log(trailerData), [trailerData]);
-
+ 
     return (
-        <>
-            Trailer
-             <VideoLink src=""/>
-        </>
-    )
+      <>
+        Trailer
+        <VideoLink src="" />
+            {trailerData && <iframe
+                width="560"
+                height="315"
+                src={`https://www.youtube.com/embed/${trailerData.results[0].key}`}
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen
+            ></iframe>}
+      </>
+    );
 }
 
 export default Trailer;
