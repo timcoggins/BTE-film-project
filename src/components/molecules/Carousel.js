b589e1eb2e6d5df23f0e7d596d8539b4a74e6aba
Tim Coggins <timcoggins@gmail.com>
2021-11-11 17:12:48 +0100

    Fixed links for carousel

--- src/components/molecules/Carousel.js
+++ src/components/molecules/Carousel.js
@@ -6,6 +6,7 @@
 
 import { Link } from 'react-router-dom'
 import { uid } from 'uid'
+import { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import H3 from '../atoms/H3'
 
@@ -38,18 +39,32 @@ const Item = styled.img`
  */
 
 const Carousel = (props) => {
+
+    const [ newData, setNewData ] = useState([])
+
+    useEffect(() => {
+        if(!props.data) return;
+            setNewData(props.data.map(item => {
+                if(item.media_type) return item;
+                item.media_type = props.media;
+                return item;
+            }))
+    }, [props.data, props.media]);
+
     return (
         <>
-            <H3>Suggested Movies</H3>
-            {props.data && <ItemContainer>
-                {props.data.map(item =>
+            {props.media === 'tv' && <H3>Similar TV Series</H3>}
+            {props.media === 'movie' && <H3>Similar Movies</H3>}
+            {props.media === undefined && <H3>Popular Content</H3>}
+            {newData && <ItemContainer>
+                {newData.map(item =>
                     <Link key={uid()} to={`/${item.media_type}/${item.id}`}>
                         <Item src={`http://image.tmdb.org/t/p/w500${item.poster_path}`} alt={item.title}/>
                     </Link>
-                    )}
+                )}
             </ItemContainer>}
         </>
     )
 }
 
-export default Carousel
+export default Carousel;
