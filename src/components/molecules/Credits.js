e6fd7cf5442c64ef95dd3d865b771be1133cf2cb
Eleonora <eleonora1weber@gmail.com>
2021-11-15 22:51:54 +0100

    added style components to credits

--- src/components/molecules/Credits.js
+++ src/components/molecules/Credits.js
@@ -1,15 +1,20 @@
+import H4 from "../atoms/H4";
+import TD from "../atoms/TD";
 
 const Credits = (props) => {
-    console.log(props)
-    return (
-      <>
-        {props.credits &&
-          props.credits.cast.slice(0, 5).map((person) => (
-            <p>{person.name}</p>
-          ))}
-      </>
-    );
-}
+  console.log(props);
+  return (
+    <>
+      <TD>
+        <H4>
+          Cast:
+          {props.credits &&
+            props.credits.cast
+              .slice(0, 5)
+              .map((person) => <p>{person.name}</p>)}
+        </H4>
+      </TD>
+    </>
+  );
+};
 export default Credits;
-
-
