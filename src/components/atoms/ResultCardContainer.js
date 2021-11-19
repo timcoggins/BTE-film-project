4f1061d53e82361ec92852205cda02fd0fa11f61
Tim Coggins <timcoggins@gmail.com>
2021-11-19 10:16:27 +0100

    Centered result cards on mobile

--- src/components/atoms/ResultCardContainer.js
+++ src/components/atoms/ResultCardContainer.js
@@ -5,8 +5,9 @@
 import styled from "styled-components";
 
 const ResultCardContainer = styled.div`
-  display: flex;
-  flex-direction: column;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
     @media screen and (min-width:500px) {
 
       justify-content: space-around;
